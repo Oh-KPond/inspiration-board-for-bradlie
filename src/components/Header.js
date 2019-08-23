dd27c262a9a3c8fa64119240ce15c9e982884a9c
Kate Pond <kate@thepondsedge.com>
2019-08-23 15:36:40 -0700

    at least the app works I guess

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { withLDConsumer } from 'launchdarkly-react-client-sdk';
 
-const Header = ({ flags }) => {
+class Header extends Component {
+  constructor({flags}) {
+    super();
 
+    console.log(flags);
+
+    this.state = {
+      flags: flags,
+    };
+  }
+
+  render(){
     let header;
 
-    console.log(flags.newHeader);
-
-    if(flags.newHeader){
+    if(this.state.flags.newHeader){
       header =
       <header className="header">
         <h1 className="header__h1">
-          <span className="header__text">An Inspiration Board for Bradlie</span>
+          <span className="header__text">An Inspiration Board for An Amazing Person</span>
         </h1>
       </header>
     }
@@ -19,9 +27,10 @@ const Header = ({ flags }) => {
       header =
       <header className="header">
         <h1 className="header__h1">
-          <span className="header__text">An Inspiration Board for An Amazing Person</span>
+          <span className="header__text">An Inspiration Board for Bradlie</span>
         </h1>
       </header>
+    }
 
     return (
       <div className="header">
@@ -30,6 +39,36 @@ const Header = ({ flags }) => {
     )
   }
 }
+
+// const Header = ({ flags }) => {
+//
+//     let header;
+//
+//     console.log(flags.newHeader);
+//
+//     if(flags.newHeader){
+//       header =
+//       <header className="header">
+//         <h1 className="header__h1">
+//           <span className="header__text">An Inspiration Board for Bradlie</span>
+//         </h1>
+//       </header>
+//     }
+//     else {
+//       header =
+//       <header className="header">
+//         <h1 className="header__h1">
+//           <span className="header__text">An Inspiration Board for An Amazing Person</span>
+//         </h1>
+//       </header>
+//
+//     return (
+//       <div className="header">
+//         {header}
+//       </div>
+//     )
+//   }
+// }
 
 
 export default withLDConsumer()(Header);
