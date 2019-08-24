a98692d8a6b6123da9053d9a100b29e7732a9bef
Kate Pond <kate@thepondsedge.com>
2019-08-24 15:50:36 -0700

    it was just a missing bracket; works now

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,74 +1,39 @@
-import React, {Component} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { withLDConsumer } from 'launchdarkly-react-client-sdk';
 
-class Header extends Component {
-  constructor({flags}) {
-    super();
+const Header = ({ flags }) => {
 
-    console.log(flags);
+  let header;
 
-    this.state = {
-      flags: flags,
-    };
+  console.log(flags.newHeader);
+
+  if(flags.newHeader){
+    header =
+    <header className="header">
+      <h1 className="header__h1">
+        <span className="header__text">An Inspiration Board for Bradlie</span>
+      </h1>
+    </header>;
+  }
+  else {
+    header =
+    <header className="header">
+      <h1 className="header__h1">
+        <span className="header__text">An Inspiration Board for An Amazing Person</span>
+      </h1>
+    </header>;
   }
 
-  render(){
-    let header;
-
-    if(this.state.flags.newHeader){
-      header =
-      <header className="header">
-        <h1 className="header__h1">
-          <span className="header__text">An Inspiration Board for An Amazing Person</span>
-        </h1>
-      </header>
-    }
-    else {
-      header =
-      <header className="header">
-        <h1 className="header__h1">
-          <span className="header__text">An Inspiration Board for Bradlie</span>
-        </h1>
-      </header>
-    }
-
-    return (
-      <div className="header">
-        {header}
-      </div>
-    )
-  }
+  return (
+    <div className="header">
+      {header}
+    </div>
+  )
 }
 
-// const Header = ({ flags }) => {
-//
-//     let header;
-//
-//     console.log(flags.newHeader);
-//
-//     if(flags.newHeader){
-//       header =
-//       <header className="header">
-//         <h1 className="header__h1">
-//           <span className="header__text">An Inspiration Board for Bradlie</span>
-//         </h1>
-//       </header>
-//     }
-//     else {
-//       header =
-//       <header className="header">
-//         <h1 className="header__h1">
-//           <span className="header__text">An Inspiration Board for An Amazing Person</span>
-//         </h1>
-//       </header>
-//
-//     return (
-//       <div className="header">
-//         {header}
-//       </div>
-//     )
-//   }
-// }
-
+Header.propTypes = {
+  flags: PropTypes.object,
+};
 
 export default withLDConsumer()(Header);
